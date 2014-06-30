fa908acdb17b27d711f712850268a561aab587f1
John <john.grindall@gmail.com>
2014-06-30 16:37:07 +0530

    Adding new files to git

---
+++ public/js/preloader.js
@@ -0,0 +1,27 @@
+App.ImageLoader = function(srcs, options){
+	this.options = options;
+	this.numLoaded = 0;
+	this.srcs = srcs;
+	this.imgs = [ ];
+	this.load();
+};
+
+App.ImageLoader.prototype.imgLoaded = function(){
+	console.log("loaded");
+	this.numLoaded++;
+	if(this.numLoaded === this.srcs.length){
+		this.options.success();
+	}
+};
+
+App.ImageLoader.prototype.load = function(){
+	var _this = this;
+	$.each(this.srcs, function(i, src){
+		var img = new Image();
+		var $img = $(img);
+		$img.on("load", $.proxy(_this.imgLoaded, _this));
+		$img.on("error", $.proxy(_this.imgLoaded, _this));
+		console.log(src);
+		$img.attr("src", src);
+	});
+};
