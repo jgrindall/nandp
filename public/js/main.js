70a2d4608812fe61a1d761c0943fb72f6714583c
John <john.grindall@gmail.com>
2014-06-30 16:34:31 +0530

    conflicts fixed

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,23 @@
 App.create = function(){
 	App.headerModel = new App.HeaderModel();
 	App.headerView = new App.HeaderView({"model":App.headerModel});
-	$("body > #header").append(App.headerView.$el);
 	App.footerView = new App.FooterView();
-	$("body > #footer").append(App.footerView.$el);
+};
+
+App.preload = function(options){
+	var srcs = ["img/logo/logosmall.png","img/logo/dog.png","img/video/video1.png","img/video/video2.png","img/clients/clients.png","img/slide/hmfs/slide1.png","img/slide/hmfs/slide2.png","img/slide/hmfs/slide3.png","img/slide/hmfs/slide4.png","img/workother/connect4_screen.jpg","img/workother/lego1.png"];
+	new App.ImageLoader(srcs, options);
 };
 
 App.init = function(){
 	App.create();
 	App.router = new App.Router();
-	Backbone.history.start();
+	App.preload({"success":function(){
+		$("body > #header").append(App.headerView.$el);
+		$("body > #footer").append(App.footerView.$el);
+		Backbone.history.start();
+	}});
 };
 
 $(document).ready(App.init);
+
