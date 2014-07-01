96d8bdb169e89912f56d32c69e9436fbb4301aac
John <john.grindall@gmail.com>
2014-07-01 17:10:42 +0530

    New stuff for simitri

--- public/js/main-min.js
+++ public/js/main-min.js
@@ -310,6 +310,7 @@ App.WorkPageView = Backbone.View.extend({
 	template:"tpl_workpage",
 	addChildren:function(){
 		var target;
+		this.v0 = new App.WorkView({"num":0});
 		this.v1 = new App.WorkView({"num":1});
 		this.v2 = new App.WorkView({"num":2});
 		this.v3 = new App.WorkView({"num":3});
@@ -317,7 +318,7 @@ App.WorkPageView = Backbone.View.extend({
 		this.v5 = new App.WorkView({"num":5});
 		this.v6 = new App.WorkView({"num":6});
 		this.v7 = new App.WorkView({"num":7});
-		this.$(".page-header").append(this.v1.$el).append(this.v2.$el).append(this.v3.$el).append(this.v4.$el).append(this.v5.$el).append(this.v6.$el).append(this.v7.$el);
+		this.$(".page-header").append(this.v0.$el).append(this.v1.$el).append(this.v2.$el).append(this.v3.$el).append(this.v4.$el).append(this.v5.$el).append(this.v6.$el).append(this.v7.$el);
 	},
 	afterAdded:function(){
 		
