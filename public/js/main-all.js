c12c543562a311483b5a86f2c988db6420288619
John <john.grindall@gmail.com>
2014-07-15 18:06:02 +0100

    tweaks

--- public/js/main-all.js
+++ public/js/main-all.js
@@ -501,14 +501,20 @@ App.OtherView = Backbone.View.extend({
 App.IntroView = Backbone.View.extend({
 	initialize:function(data){
 		this.render();
+		setTimeout($.proxy(this.rot, this), 1000);
+		this.interval = setInterval($.proxy(this.rot, this), 8000);
+	},
+	rot:function(){
+		this.$rot.toggleClass("rot");
 	},
 	template:"tpl_intro",
 	render:function(){
 		this.loadTemplate(this.template, {}, {replace:true} );
+		this.$rot = this.$(".biohazard");
 		return this;
 	},
 	beforeClose:function(){
-		
+		clearInterval(this.interval);
 	}
 });
 
--- public/js/views/intro.js
+++ public/js/views/intro.js
@@ -3,14 +3,20 @@
 App.IntroView = Backbone.View.extend({
 	initialize:function(data){
 		this.render();
+		setTimeout($.proxy(this.rot, this), 1000);
+		this.interval = setInterval($.proxy(this.rot, this), 8000);
+	},
+	rot:function(){
+		this.$rot.toggleClass("rot");
 	},
 	template:"tpl_intro",
 	render:function(){
 		this.loadTemplate(this.template, {}, {replace:true} );
+		this.$rot = this.$(".biohazard");
 		return this;
 	},
 	beforeClose:function(){
-		
+		clearInterval(this.interval);
 	}
 });
 
