49c4e7a8a58c726b79bf12f6b53533fa6b4d9f9e
John <john.grindall@gmail.com>
2016-04-28 21:12:04 +0100

    new version and images

--- public/js/views/otherpage.js
+++ public/js/views/otherpage.js
@@ -11,7 +11,7 @@ App.OtherPageView = Backbone.View.extend({
 		this.v3 = new App.OtherView({"num":3});
 		this.v4 = new App.OtherView({"num":4});
 		this.v5 = new App.OtherView({"num":5});
-		this.$(".page-header").append(this.v1.$el).append(this.v2.$el).append(this.v3.$el).append(this.v4.$el).append(this.v5.$el);
+		this.$(".page-header").append(this.v3.$el).append(this.v2.$el).append(this.v1.$el).append(this.v4.$el).append(this.v5.$el);
 	},
 	render:function(){
 		this.loadTemplate(this.template, {}, {replace:true} );
