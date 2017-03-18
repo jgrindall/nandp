bdcfc2db42d1b400ef9a33855157b10198eca712
John <john.grindall@gmail.com>
2017-03-18 14:16:35 +0000

    new images + script

---
+++ public/img/workother/logo/list.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0187.PNG ./ss1.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0190.PNG ./ss2.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0194.PNG ./ss3.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0195.PNG ./ss4.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0197.PNG ./ss5.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0204.PNG ./ss6.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0206.PNG ./ss7.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0207.PNG ./ss8.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0214.PNG ./ss9.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0215.PNG ./ss10.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0217.PNG ./ss11.png
+convert -composite -gravity center ipad.png ../../../../../../ios/LogotacularIOS/raw/imgs/IMG_0225.PNG ./ss12.png
+
